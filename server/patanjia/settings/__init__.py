561421bb0675ee4ea7132074a9cb45119388debf
Zachary Waweru <actuaryzask@gmail.com>
2021-10-19 16:24:44 +0300

    had forgotten to turn Production to true on server

--- server/patanjia/settings/__init__.py
+++ server/patanjia/settings/__init__.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-PRODUCTION = False
+PRODUCTION = True
 
 # production settings
 if PRODUCTION:
