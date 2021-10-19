7395960d8a4063ca00333124042b50c04a184813
Zachary Waweru <actuaryzask@gmail.com>
2021-10-19 17:25:12 +0300

    added react urls

--- server/patanjia/urls.py
+++ server/patanjia/urls.py
@@ -1,6 +1,13 @@
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+# import the settings to check the PRODUCTION status to facilitate file serving
+from .settings import PRODUCTION
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +16,15 @@ urlpatterns = [
     path('api/user/', include('user.api.urls')),
     path('api/search/', include('search.api.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# if not production serve files locally
+if not PRODUCTION:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
+# to allow for incorporation of react front end urls/ other routes not represented above
+
+urlpatterns += [re_path(r'^.*',
+                        TemplateView.as_view(template_name='index.html'))]
