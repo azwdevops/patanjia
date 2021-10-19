d33186a66045a3eeae5024dbe970080850872ad3
Zachary Waweru <actuaryzask@gmail.com>
2021-10-19 17:42:40 +0300

    removed uuid from title search url to avoid errors

--- server/search/api/urls.py
+++ server/search/api/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = (
     path('maintain-titles/<uuid:userId>/',
          views.TitleView.as_view(), name='maintain_titles'),
-    path('valuer-get-title/<uuid:userId>/',
+    path('valuer-get-title/<userId>/',
          views.ValuerTitleView.as_view(), name='valuer_get_title'),
 )
